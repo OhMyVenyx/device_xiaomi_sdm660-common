a1fa87f4f59ba0ec88f42346643777551eda71ba
nullxception <enuma.alrizky@gmail.com>
2021-08-18 18:48:34 +0000

    sdm660-common: init: Add script for exTHmUI's GameMode performance control
    This is based on samples at exTHmUI-supported device (link down below),
    but without turing-complete language involvements since we're only
    setting up sysfs nodes.
    
    [fryevia: correct wrong thermal value and add preset for inactive mode]
    
    Link: exthmui-devices/android_device_xiaomi_vince@dc18f8e
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <valkry24@gmail.com>
    Signed-off-by: anuragbhaumik <bhaumik463@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Signed-off-by: Lucifer-morning-star-96 <MSnell3130@student.egcc.edu>
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>
    Signed-off-by: Rahmad Adi Pratama <OhMyVenyx@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -289,6 +289,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.performance_level.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
