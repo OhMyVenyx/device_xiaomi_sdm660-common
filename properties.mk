877b39c211968da390af075ff25799649f225fb3
Rahmad Adi Pratama <OhMyVenyx@proton.me>
2023-06-26 09:23:39 +0700

    Revert "sdm660-common: Enable blurs, under dev-option"
    This reverts commit cbff17780db23975265c83d87586b55a57b4a725.

--- properties.mk
+++ properties.mk
@@ -99,12 +99,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
-# System Blur
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.sf.disable_blurs=1 \
-    ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1
-
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.preview.ubwc=0 \
