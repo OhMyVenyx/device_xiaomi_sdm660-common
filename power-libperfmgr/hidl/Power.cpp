mitmac29/android_device_xiaomi_sdm660-common

sdm660-common: power-libperfmgr: Add support for second tap-to-wake node
Clover has a different dt2w node

Change-Id: Ic4f2a132b8a102eb01edab86469eccd7dfcd6c84
Co-authored-by: pix106 <sbordenave@gmail.com>
Signed-off-by: pix106 <sbordenave@gmail.com>

power-libperfmgr/aidl/Power.cpp
@@ -33,6 +33,10 @@
 #define TAP_TO_WAKE_NODE "/sys/touchpanel/double_tap"
 #endif
 
+#ifndef TAP_TO_WAKE_NODE2
+#define TAP_TO_WAKE_NODE2 "/proc/touchscreen/enable_dt2w"
+#endif
+
 namespace aidl {
 namespace google {
 namespace hardware {
@@ -76,6 +80,7 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
         case Mode::DOUBLE_TAP_TO_WAKE:
             {
             ::android::base::WriteStringToFile(enabled ? "1" : "0", TAP_TO_WAKE_NODE, true);
+            ::android::base::WriteStringToFile(enabled ? "1" : "0", TAP_TO_WAKE_NODE2, true);
             [[fallthrough]];
             }
         case Mode::LOW_POWER:

OhMyVenyx/device_xiaomi_sdm660-common

Signed-off-by: Rahmad Adi Pratama <OhMyVenyx@proton.me>

power-libperfmgr/hidl/Power.cpp
@@ -37,6 +37,10 @@
 #define TAP_TO_WAKE_NODE "/sys/touchpanel/double_tap"
 #endif
 
+#ifndef TAP_TO_WAKE_NODE2
+#define TAP_TO_WAKE_NODE2 "/proc/touchscreen/enable_dt2w"
+#endif
+
 extern struct stat_pair rpm_stat_map[];
 
 namespace android {
@@ -153,6 +157,7 @@ Return<void> Power::setFeature(Feature feature, bool activate)  {
 #ifdef TAP_TO_WAKE_NODE
         case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE:
             ::android::base::WriteStringToFile(activate ? "1" : "0", TAP_TO_WAKE_NODE, true);
+            ::android::base::WriteStringToFile(activate ? "1" : "0", TAP_TO_WAKE_NODE2, true);
             break;
 #endif
         default: